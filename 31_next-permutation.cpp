62829935c1c5a9c5fc9503f9aac8ec6027f4ba12
Hanif <mohammadabuhanif95@gmail.com>
2025-12-30 22:48:27 +0800

    solve LeetCode 31 - Next Permutation
    Algorithm steps:
    1. Find first decreasing element from right (pivot)
    2. If no pivot, reverse entire array (last permutation case)
    3. Find element just larger than pivot from right and swap
    4. Reverse suffix after pivot position
    
    Time Complexity: O(n)
    Space Complexity: O(1)

---
+++ 31_next-permutation.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+    int n = nums.size();
+    
+    // Find pivot
+    int pivot = -1;
+    for (int i = n - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+            pivot = i;
+            break;
+        }
+    }
+    
+    if (pivot == -1) {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+    
+    // Find element to swap with pivot
+    for (int i = n - 1; i > pivot; i--) {
+        if (nums[i] > nums[pivot]) {
+            swap(nums[i], nums[pivot]);
+            break;
+        }
+    }
+    
+    // Reverse suffix
+    reverse(nums.begin() + pivot + 1, nums.end());
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    
+    cout << "Before: ";
+    for (int num : nums) cout << num << " ";
+    cout << endl;
+    
+    nextPermutation(nums);
+    
+    cout << "After: ";
+    for (int num : nums) cout << num << " ";
+    cout << endl;
+    
+    return 0;
+}
