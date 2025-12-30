04795c0a345d987012e94c4f9063362148350bee
Hanif <mohammadabuhanif95@gmail.com>
2025-12-30 20:40:40 +0800

    Solve LeetCode 88: Merge Sorted Array
    - Implement O(m+n) time, O(1) space solution for merging two sorted arrays
    - Use three-pointer approach starting from end of arrays

---
+++ 88_merge_sorted_array.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+    int idx = m+n -1, i = m-1, j = n-1;
+
+    while(i >= 0 && j >= 0){
+        if(nums1[i] >= nums2[j]){
+            nums1[idx--] = nums1[i--];
+        } else {
+            nums1[idx--] = nums2[j--];
+        }
+    }
+
+    while(j >= 0){
+        nums1[idx--]  = nums2[j--];
+    }
+}
+
+void printVector(const vector<int>& nums){
+    cout << "[";
+    for(int i=0; i<nums.size(); i++){
+        cout << nums[i];
+        if(i < nums.size() - 1){
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+}
+
+int main(){
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums2 = {2, 5, 6};
+    int m = 3, n = 3;
+
+    merge(nums1, m, nums2, n);
+    printVector(nums1);
+    cout << endl;
+
+    return 0;
+}
