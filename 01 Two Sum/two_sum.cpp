8f3920d7c7c92085d963d2139aed761cdeb13b99
Hanif <mohammadabuhanif95@gmail.com>
2026-02-04 22:55:35 +0800

    LeetCode:01 Two Sum problem Solve using c++ Implemented efficient O(n) solution using unordered_map (hash table)

---
+++ 01 Two Sum/two_sum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target){
+    unordered_map<int, int> map;
+
+    for(int i = 0; i<nums.size(); i++){
+        int complement = target - nums[i];
+        if(map.find(complement) != map.end()){
+            return {map[complement], i};
+        }
+        map[nums[i]] = i;
+    }
+    return {};
+}
+
+int main(){
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+   
+    vector<int> ans= twoSum(nums, target);
+    cout <<"Output : [" << ans[0] << ", " << ans[1] << "]" <<endl;
+
+    return 0;
+}
