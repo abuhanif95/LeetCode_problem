301e059f385eab5b9b28db5796f7563b60c82649
Hanif <mohammadabuhanif95@gmail.com>
2026-02-12 23:44:42 +0800

    Solve the LeetCode problem 240: Search a 2D Matrix II
    -Implement efficient O(m+n) solution using staircase search
    -Start from top-right corner and eliminate row or column based on target comparison

---
+++ 240 Search a 2D Matrix II/search_2D_matrix_II.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        
+        int r = 0, c = n-1;
+        while(r < m && c >=0){
+            if(target == matrix[r][c]){
+                return true;
+            }else if(target < matrix[r][c]){
+                c--;
+            }else{
+                r++;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    vector<vector<int>> matrix = {
+        {1, 4, 7, 11, 15},
+        {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    
+    int target1 = 5;
+    cout << solution.searchMatrix(matrix, target1) << endl;
+    
+    int target2 = 20;
+    cout << solution.searchMatrix(matrix, target2) << endl;
+    
+    return 0;
+}
