d5aba0d6217485fce4eb32943e40d70f3dab24f1
sfthanif24 <178651487+sfthanif24@users.noreply.github.com>
2026-04-18 17:59:52 +0800

    Solve Two Sum (LeetCode 1) using optimal hash map technique
    - Implements unordered_map for O(1) lookups
    - Single pass iteration instead of nested loops
    - Reduces time complexity from O(n²) to O(n)
    - Handles edge cases like duplicate numbers correctly

--- C++/01_Two_Sum.cpp
+++ C++/01_Two_Sum.cpp
@@ -6,22 +6,28 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target){
-    unordered_map<int, int> map;
+        vector<int> ans;
+        unordered_map<int, int> map;
 
-    for(int i = 0; i<nums.size(); i++){
-        int complement = target - nums[i];
-        if(map.find(complement) != map.end()){
-            return {map[complement], i};
-        }
-        map[nums[i]] = i;
+        for(int i = 0; i<nums.size(); i++){
+            int first = nums[i];
+            int second = target - first;
+
+            if(map.find(second) != map.end()){
+                ans.push_back(map[second]);
+                ans.push_back(i);
+                break;
+           }
+        map[first] = i;
+       }
+    return ans;
     }
-    return {};
-    }
-}; 
+};
 
 int main(){
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;
+
    
     Solution test;
     vector<int> ans = test.twoSum(nums, target);
